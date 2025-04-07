0e42032640cd5f70276c565f30bf0f79f06346ea
Andres Aguilar <aguilarcarboni@gmail.com>
2025-04-07 11:29:08 -0600

    Update name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { Analytics } from "@vercel/analytics/react"
 import Header from '@/components/Header'
 
 export const metadata: Metadata = {
-  title: 'Andrés Aguilar Carboni',
+  title: 'Andrés Aguilar',
   description: 'Software Engineer and Data Analyst',
 };
 
