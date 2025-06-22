fd0f34220feb030838464f8dd5090f9c977b5ed3
Andres Aguilar <aguilarcarboni@gmail.com>
2025-06-22 09:12:16 -0600

    Update bio content for clarity and detail, enhancing descriptions of personal experience and passion.

--- src/lib/resume/bio.ts
+++ src/lib/resume/bio.ts
@@ -4,13 +4,13 @@ export const bio = [
     { 
       title: "Get to know me", 
       description: "A little about myself", 
-      content: "I am a software engineer with hands-on experience on front-end web development, back-end engineering, database systems, devops, and more currently working towards a Bachelor’s of Science in Computer Science with a minor in Mathematics.", 
+      content: "I am a software engineer with hands-on experience on front-end web development, back-end engineering, database systems, devops, and anything else you can think of. I'm currently working towards a Bachelor’s of Science in Computer Science with a minor in Mathematics at Texas Tech University.", 
       icon: User 
     },
     {
       title: "My passion",
       description: "Pushing to grow",
-      content: "I pride myself on the excellence that I approach every project I work on, whether it be professional or personal. When a project interests and challenges me, I am extremely passionate and will work as hard as necessary to grow, learn and make it a reality while helping teammates around me rise up as well.",
+      content: "I pride myself on the excellence that I approach every project I work on, whether it be personal or professional. When a project interests and challenges me I am extremely passionate and will work as dilligently as needed to grow, learn and adapt while helping teammates around me rise up as well.",
       icon: Briefcase
     },
     { 
