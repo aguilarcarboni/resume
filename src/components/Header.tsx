0c27eebdbf38d1c6e549d112b099edc1d408e665
Andres Aguilar <aguilarcarboni@gmail.com>
2025-06-23 17:57:52 -0600

    Remove outdated logo assets and update Header component to use new icon for improved branding consistency.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className='flex items-center justify-between w-full h-fit'>
         <Link href="/" className='cursor-pointer'>
-          <Image src="/assets/brand/lf-logo-transparent.png" alt="@laserfocus" width={75} height={75}/>
+          <Image src="/assets/brand/icon.png" alt="@laserfocus" width={75} height={75}/>
         </Link>
     </div>
   )
